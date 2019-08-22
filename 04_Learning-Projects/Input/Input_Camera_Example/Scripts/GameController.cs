75f362ebfb8779c6fc2df8b226f83f4e264e5c12
Mustafa <mustafakatipoglu98@hotmail.com>
2019-08-22 03:41:14 +0300

    Input and Camera Example

---
+++ 04_Learning-Projects/Input/Input_Camera_Example/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject[] gameCameras;
+    private int _cameraIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnFocusCamera(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetMouseButtonDown(0))
+            ChangeCamera(1);
+        if( Input.GetMouseButtonDown(1))
+            ChangeCamera(-1);
+    }
+
+    void OnFocusCamera(int index)
+    {
+        for (int i = 0; i < gameCameras.Length; i++)
+            gameCameras[i].SetActive( false );
+        gameCameras[index].SetActive(true);
+    }
+
+    void ChangeCamera(int direction) {
+        _cameraIndex = (_cameraIndex + direction + gameCameras.Length) % gameCameras.Length;
+        OnFocusCamera(_cameraIndex);
+    }
+}
---
+++ 04_Learning-Projects/Input/Input_Camera_Example/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 100f;
+    public float angularSpeed = 40f;
+    private Rigidbody _rigidbody = null;
+    private bool jumpable = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKey("left"))
+            transform.RotateAround(transform.position, Vector3.up, -angularSpeed * Time.deltaTime );
+        else if( Input.GetKey("right"))
+            transform.RotateAround(transform.position, Vector3.up, angularSpeed * Time.deltaTime );
+        else if( Input.GetKey("up"))
+            transform.position += transform.forward * ( speed * Time.deltaTime);
+        else if( Input.GetKey("down"))
+            transform.position += transform.forward * ( -speed * Time.deltaTime);
+        else if (Input.GetKey("space") && _rigidbody != null && jumpable)
+        {
+            jumpable = false;
+            _rigidbody.AddForce(0, jumpForce, 0);
+        }    
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.name == "Floor")
+            jumpable = true;
+    }
+}
