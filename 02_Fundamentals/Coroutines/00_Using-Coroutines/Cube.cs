6b00027da0698bbc4502836a06ac524bb11b49d4
Mustafa <mustafakatipoglu98@hotmail.com>
2019-08-23 08:36:12 +0300

    Coroutines

---
+++ 02_Fundamentals/Coroutines/00_Using-Coroutines/Cube.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Test : MonoBehaviour {
+    private IEnumerator _currentMoveCoroutine;
+    
+    // Start is called before the first frame update
+    void Start() {
+        string[] msgs = { "Bu", "Bir", "Bant", "Kaydidir..."};
+        StartCoroutine(PrintMessage(msgs, 1));
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if( _currentMoveCoroutine != null )
+                StopCoroutine(_currentMoveCoroutine);
+            _currentMoveCoroutine = Move(Random.onUnitSphere * 5, 1);
+            StartCoroutine(_currentMoveCoroutine);
+        }
+    }
+
+    IEnumerator PrintMessage(string[] messages, float delay) {
+        foreach (var message in messages) {
+            print(message);
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    IEnumerator Move(Vector3 destionation, float speed) {
+        while ( transform.position != destionation ) {
+            transform.position = Vector3.MoveTowards(transform.position, destionation, speed * Time.deltaTime);
+            yield return null;            // Pause until next frame
+        }
+    }
+
+
+}
---
+++ 02_Fundamentals/Coroutines/00_Using-Coroutines/Cylinder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cylinder : MonoBehaviour {
+    public WaypointNetwork waypointNetwork;
+    
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(FollowPoints());
+    }
+
+    IEnumerator FollowPoints() {
+        foreach (var waypoint in waypointNetwork.waypoints)
+            yield return Move(waypoint.transform.position,1);        // Pause until function returns
+    }
+    
+    
+    IEnumerator Move(Vector3 destionation, float speed) {
+        while ( transform.position != destionation ) {
+            transform.position = Vector3.MoveTowards(transform.position, destionation, speed * Time.deltaTime);
+            yield return null;            // Pause until next framme
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
